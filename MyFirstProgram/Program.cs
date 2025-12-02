245a2bc64b41ec058ae24762a94de4f103640765
AXL-29 <gimpaojade4@gmail.com>
2025-12-02 13:03:05 +0800

    Learn about Console.WriteLine() which used to print output to the console

--- MyFirstProgram/Program.cs
+++ MyFirstProgram/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!"); // Console.WriteLine() is used to print text to the console.
+    }
+}
