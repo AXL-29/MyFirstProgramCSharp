1a13154af0ded111353fcc1164a9cddf064968c9
AXL-29 <gimpaojade4@gmail.com>
2025-12-02 13:09:12 +0800

    Learn about Console.Beep() that produces a beep sound from the console.

--- MyFirstProgram/Program.cs
+++ MyFirstProgram/Program.cs
@@ -3,5 +3,6 @@
     public static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!"); // Console.WriteLine() is used to print text to the console.
+        Console.Beep(); // Console.Beep() produces a beep sound from the console.
     }
 }
