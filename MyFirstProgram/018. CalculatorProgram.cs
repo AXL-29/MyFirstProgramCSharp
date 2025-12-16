722f976d36741e962f89da93620b81bd915b6306
Jade Mark Gimpao <gimpaojade4@gmail.com>
2025-12-16 13:37:56 +0800

    Add calculator logic using switch and while loops

--- MyFirstProgram/018. CalculatorProgram.cs
+++ MyFirstProgram/018. CalculatorProgram.cs
@@ -1,1 +1,101 @@
+using System;
 
+class CalculatorProgram
+{
+    static void Main(string[] args)
+    {
+        bool isCalculatorOn = true;
+        string mathOperator = "";
+        double result;
+        
+        while (isCalculatorOn)
+        {
+            Console.Write("Enter a first number: ");
+            double firstNum = Convert.ToInt32(Console.ReadLine());
+
+            while(true)
+            {       
+                Console.Write("Enter an operator (+, -, *, /, or %): ");
+                mathOperator = Console.ReadLine();
+
+                if (
+                    (mathOperator == "+") ||
+                    (mathOperator == "-") ||
+                    (mathOperator == "*") ||
+                    (mathOperator == "/") ||
+                    (mathOperator == "%"))
+                {
+                    break;
+                }
+                Console.WriteLine("Please typed (+, -, * /, or %) only.");  
+            }
+
+            Console.Write("Enter a second number: ");
+            double secondNum = Convert.ToInt32(Console.ReadLine());
+
+            switch (mathOperator)
+            {
+                case "+":
+                result = firstNum + secondNum;
+                Console.WriteLine("The sum of two numbers is: " + Math.Round(result, 2));
+                break;
+
+                case "-":
+                result = firstNum - secondNum;
+                Console.WriteLine("The difference of two numbers is: " + Math.Round(result, 2));
+                break;
+
+                case "*":
+                result = firstNum * secondNum;
+                Console.WriteLine("The product of two numbers is: " + Math.Round(result, 2));
+                break;
+
+                case "/":
+                    if (secondNum == 0)
+                    {
+                        Console.WriteLine("Sorry, the first number cannot be divided by zero");
+                        break;
+                    }
+                    else
+                    {
+                        result = firstNum + secondNum;
+                        Console.WriteLine("The sum of two numbers is: " + Math.Round(result, 2));
+                        break;                        
+                    }
+                
+                case "%":
+                result = firstNum % secondNum;
+                Console.WriteLine("The remainder of two numbers is: " + Math.Round(result, 2));
+                break;
+            }
+            
+            Console.WriteLine();
+
+            while (true)
+            {
+                Console.Write("Do you want to calculate again? (y/n): ");
+                string calculateAgain = Console.ReadLine().ToLower();
+
+                if (calculateAgain == "y")
+                {
+                    break;
+                }
+                else if (calculateAgain == "n")
+                {
+                    Console.WriteLine("Thank you for using our calculator app!");
+                    isCalculatorOn = false;
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Please choose between 'y' or 'n' only!");
+                }
+            }
+            
+        Console.WriteLine();
+
+        }
+
+        Console.ReadKey();
+    }
+}
